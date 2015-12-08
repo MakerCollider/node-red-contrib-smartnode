b795543992451acc4995f05a27430a5a97d5493f
Tian Ye <flymaxty@gmail.com>
2015-12-08 20:17:58 +0800

    Add unplugged detect on camera node

--- imageProcessing/camera/camera.js
+++ imageProcessing/camera/camera.js
@@ -52,9 +52,24 @@ module.exports = function(RED) {
         var camera = new jslib.Camera(node.cameraId, node.weidth, node.height);
 
         function camera_timer(){
-        	var ptrString = camera.read();
-            var msg =  {imagePtr:ptrString};
-            node.send(msg);
+		var isVaild = true;
+		if(camera.m_running){
+        		var ptrString = camera.read();
+			if(ptrString == ""){
+				isVaild = false;
+			}
+            		var msg =  {imagePtr:ptrString};
+            		node.send(msg);
+		}
+		else{
+			isVaild = false;
+		}
+		
+		if(!isVaild){
+			node.log("Camera unplugged");
+			clearInterval(node.timer);
+			node.status({fill:"red", shape:"dot", text:"Unplugged"});
+		}
         }
 
         node.log("Camera prepared.");

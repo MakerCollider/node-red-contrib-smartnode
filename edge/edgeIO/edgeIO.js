66ea52086f24e75ed7065f423d7183e4b6102db1
guozhong123 <guozhong2000@126.com>
2016-05-27 16:32:22 +0800

    add edge node
    add edge node

---
+++ edge/edgeIO/edgeIO.js
@@ -0,0 +1,78 @@
+module.exports = function(RED) {
+    var fs = require("fs");
+    var file = __dirname + '/../profile.json'; 
+
+    function edgeInNode(config) {
+        this.name = config.name;
+        this.service = config.service;
+        this.attribute = config.attribute;
+        var node = this;
+        RED.nodes.createNode(node, config);
+        var profile = JSON.parse(fs.readFileSync(file));
+
+        edge.on('write', function(serviceName, attrName, val) {
+            var edge = {
+                "service": serviceName,
+                "attr": attrName,
+                "value": val
+            };
+            console.log('++++++++++');
+            console.log(edge);
+            var flag = false;
+            if (node.service == serviceName && node.attribute == attrName){
+                flag = true;
+            }
+            if (checkEdgeProfile(profile,edge) && flag == true){
+                msg.payload = val;
+                node.send(msg);
+            } 
+        });
+    }
+
+    RED.nodes.registerType("edgeIn", edgeInNode);
+
+
+    function edgeOutNode(config) {     
+        this.name = config.name;
+        this.service = config.service;
+        this.attribute = config.attribute;
+        var node = this;
+        RED.nodes.createNode(node, config);
+        var profile = JSON.parse(fs.readFileSync(file));
+        console.log('+++++++++++++++++++++++');
+        //console.log(profile);
+
+        node.on('input', function(msg) {
+            var output = {
+                'service':node.service,
+                'attr':node.attribute,
+                'value':msg.payload
+            };
+
+            if (checkEdgeProfile(profile,output)){
+                edge.set(output.service, output.attr, output.value);
+                edge.get(output.service,output.attr,function(result){
+                    console.log(result);
+                });
+            }
+        });
+
+    }
+
+    RED.nodes.registerType("edgeOut", edgeOutNode);
+
+    function checkEdgeProfile(profile,edge){
+        for (var dev in profile) {
+            if (dev != 'dev_info'){
+                for(var attr in profile[dev]){
+                    if (edge.service == dev && edge.attr == attr){
+                        return true;
+                    }
+                } 
+            }
+
+        }
+        return false;
+    }
+}
+

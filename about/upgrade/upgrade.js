e2aef64d68f07ca6459effc733748c3f5a03f2f2
guozhong123 <guozhong2000@126.com>
2016-07-07 16:20:48 +0800

    Update node upgrade  change  ftp to http
    Update node upgrade  change  ftp to http

--- about/upgrade/upgrade.js
+++ about/upgrade/upgrade.js
@@ -6,152 +6,170 @@ module.exports = function(RED){
         var node = this;
 
         var cp = require('child_process');
-        var fs = require('fs');
-        var fse = require('fs-extra')
         var path = require('path');
-        //use ftp get remote files list
-        var ftpClient = require('ftp');
-        
-        var dirPath = '/SmartNode_Build/Stable/';
-        var ftpOptions = {'host':'114.215.140.168'};
+        var fs = require('fs');
+
         var versionName = '';
         var rootDir = getCurrDirPath();
 
-        function ftpGetVersionLists(ws){
-            var ftpc = new ftpClient();
-            ftpc.on('ready', function() {
-                fs.readFile(rootDir+'version','utf-8',function(err,data){  
-                    if (err){
-                        console.log("read version error");  
+        //--------------http---------------
+        var http = require('http');
+        var qs = require('querystring');
+
+        var versionLists = [];
+        var rowArr = [];
+        var options = {
+            hostname: 'repository.smartnode.io',
+            port: 80,
+            path: '/download/Stable/',
+            method: 'GET'
+        };
+
+        var req = http.request(options, function (res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                //console.log('BODY: ' + chunk);
+                var start = chunk.indexOf('<table>');
+                var stop = chunk.indexOf('</table>');
+                chunk =  chunk.substring(start+7,stop);
+
+                var rows = chunk.split('</tr>');
+                for(var i=0;i<rows.length;i++){
+                    rows[i] = rows[i]+'</tr>';
+                    if (rows[i].indexOf('SmartNode')!=-1){
+                        rowArr.push(rows[i]);
                     }
-                    else{  
-                        console.log(data);
-                        ws.send('{current_version}'+data);
-                    }  
-                });
+                }
 
-                ftpc.list(dirPath,function(err, list) {
-                    if (err) {
-                        throw err;
-                        ws.send('{version_list_error}加载失败');
-                    }
-                    //console.dir(list);
-                    var fileArray = new Array();
-                    for (var i in list) {
-                        var fileList = {};
-                        fileList.id = i;
-                        fileList.name = list[i].name;
-                        fileList.size = list[i].size;
-
-                        if (filterStableVersion(list[i].name)){
-                            fileArray.push(fileList);
-                        }
-                    };
-
-                    console.log(JSON.stringify(fileArray));
-                    ws.send('{versions}'+JSON.stringify(fileArray));
-                    ws.send('{version_list_finished}加载完成');
-                    ftpc.end();  
-                });
+                for (var i = 0; i < rowArr.length; i++) {
+                    var versions = {};
+                    versions = splitTr(rowArr[i]);
+                    if (!isEmptyObject(versions)){
+                       versionLists.push(versions);
+                    } 
+                }
+                
+                for(var ver in versionLists){
+                    //parse name
+                    var _verName = versionLists[ver].name;
+                    var start1 = _verName.indexOf('href="');
+                    var stop1 = _verName.indexOf('">');
+                    _verName =  _verName.substring(start1+6,stop1);
+                    versionLists[ver].name = trim(_verName);
+                    
+                    //parse size
+                    var _verSize = versionLists[ver].size;
+                    var start2 = _verSize.indexOf('">');
+                    var stop2 = _verSize.indexOf('</td>');
+                    _verSize =  _verSize.substring(start2+2,stop2);
+                    versionLists[ver].size = trim(_verSize);
+                }
+                //console.log(versionLists);
             });
-            // connect to localhost:21 as anonymous
-            ftpc.connect(ftpOptions);
-        }
 
+            function splitTr(row){
+                var _versions = {};
+                var cols = row.split('</td>');
+                for(var i=0;i<cols.length;i++){
+                    cols[i] = cols[i]+'</td>';
+                    if (i == 1){
+                        _versions.name = cols[i];
+                    }
+                    if (i == 3){
+                        _versions.size = cols[i];
+                    }
+                }
+
+                if (_versions.hasOwnProperty("name") && _versions.hasOwnProperty("size")){
+                    return _versions;
+                }
+                return {};
+            }
+
+            function trim(str) {
+                str = str.replace(/^(\s|\u00A0)+/, '');
+                for (var i = str.length - 1; i >= 0; i--) {
+                    if (/\S/.test(str.charAt(i))) {
+                        str = str.substring(0, i + 1);
+                        break;
+                    }
+                }
+                return str;
+            }
+
+            function isEmptyObject(e) {  
+                var t;  
+                for (t in e)  
+                    return !1;  
+                return !0  
+            }
+        });
+        req.on('error', function (e) {
+            console.log('problem with request: ' + e.message);
+        });
+        req.end();
+        //-----------------http---------------------
         function getCurrDirPath(){
             var rootDir = path.dirname(__filename)+ '/../../../../../';
             return rootDir;
         }
-        
-        function filterStableVersion(versionName){
-            //versionName = 'SmartNode-1.1.3.install';
-            var version = versionName.replace('SmartNode-','');
-            version = version.replace('.install','');
-            var data = version.split('.');
-            if (parseInt(data[0])>0 && parseInt(data[1])>=1){
-                if (parseInt(data[2])>=4){
-                    return true;
+
+        function getVersionLists(ws){
+            fs.readFile(rootDir+'version','utf-8',function(err,data){  
+                if (err){
+                    console.log("read version error");  
                 }
-                else{
-                    return false;
+                else{  
+                    //console.log(data);
+                    ws.send('{current_version}'+data);
+                }  
+            });
+
+            ws.send('{versions}'+JSON.stringify(versionLists));
+            ws.send('{version_list_finished}加载完成');  
+        }
+
+        function downLoad(ws,file){
+            if (versionName.length == 0){
+                versionName = file.name;
+            }
+            var versionPath = options.hostname+options.path+versionName;
+            var cmd = 'cd '+rootDir+' && wget -c -O '+versionName+' '+versionPath;
+            console.log(cmd);
+            
+            var _options =  {
+                encoding: 'utf8',
+                timeout: 0,
+                maxBuffer: 1000*1024,
+                killSignal: 'SIGTERM',
+                cwd: null,
+                env: null
+            };
+            var ls = cp.exec(cmd,_options);
+
+            ls.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+            });
+
+            ls.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+                if (data.indexOf('%')!=-1){
+                    data = data.replace(/\./g,'');
+                    var progress_txt = '{progress}已完成' + data;
+                    ws.send(progress_txt);
                 }
-            }
-            else{
-                return false;
-            }
-        }
-
-        function ftpDownLoad(ws,file){
-            versionName = file.name;
-            var upgradePath = path.dirname(__filename)+ '/../../../../../';
-            var writeStream = fs.createWriteStream(upgradePath+file.name);
-            var totalSize = file.size;
-
-            var passedLength = 0;
-            var lastSize = 0;
-            var startTime = Date.now();
-            //out = process.stdout;
-
-            var ftpc = new ftpClient();
-            ftpc.on('ready', function() {
-
-                var filePath = dirPath+file.name;
-                ftpc.get(filePath, function(err, stream) {
-                    if (err) throw err;
-                    stream.once('close', function() { ftpc.end(); });
-                    //stream.pipe(fs.createWriteStream('SmartNode-1.1.3.install'));
-
-                    stream.on('data', function(chunk) { // 当有数据流出时，写入数据
-                        passedLength += chunk.length;
-                        if (writeStream.write(chunk) === false) { // 如果没有写完，暂停读取流
-                            stream.pause();
-                        }
-                    });
-
-                    writeStream.on('drain', function() { // 写完后，继续读取
-                        stream.resume();
-                        
-                        var percent = Math.ceil((passedLength / totalSize) * 100);
-                        var size = Math.round((passedLength / (1024*1024))*100)/100;
-                        var diff = Math.round((size - lastSize)*100)/100;
-                        lastSize = size;
-                        //out.clearLine();
-                        //out.cursorTo(0);
-                        console.log('已完成' + size + 'MB, ' + percent + '%, 速度：' + diff * 2 + 'MB/s');
-                        var progress_txt = '{progress}已完成' + size + 'MB, ' + percent + '%, 速度：' + diff * 2 + 'MB/s';
-                        ws.send(progress_txt);
-                    });
-
-                    stream.on('end', function() { // 当没有数据时，关闭数据流
-                        writeStream.end();
-                        ftpc.end();
-                        var endTime = Date.now();
-                        var totalTime = '共用时：' + Math.ceil((endTime - startTime) / 1000) + '秒。';
-                        ws.send(totalTime);
-                        ws.send('{download_finished}下载完成！');
-                    });
-                });
             });
-            // connect to localhost:21 as anonymous
-            ftpc.connect(ftpOptions);
-        }
 
-        function installSmartNode(ws,file){
-            backUpOldVersion(ws,file);
-        }
+            ls.on('exit', function (code) {
+                console.log('exit:child process exited with code ' + code);
+            }); 
 
-        function backUpOldVersion(ws,file){
-            ws.send('{backup_start}正在备份当前版本...');
-            fse.copy('./', '../bakup', function (err) {
-              if (err){
-                 ws.send('{backup_error}备份失败！');
-                 console.error(err);
-              }
-              console.log('success!');
-              ws.send('{backup_finished}备份完成！');
-              //install
-              install(ws,file);
-            });
+            ls.on('close', function (code) {
+                console.log('close:child process exited with code ' + code);
+                ws.send('{download_finished}下载完成！');
+            }); 
         }
 
         function install(ws,file){
@@ -179,10 +197,14 @@ module.exports = function(RED){
 
             ls.on('exit', function (code) {
                 console.log('child process exited with code ' + code);
+            }); 
+
+            ls.on('close', function (code) {
+                console.log('child process exited with code ' + code);
                 if (installStatus == 1){
                     installExtract(ws,versionName);
                 }
-            }); 
+            });
         }
 
         function installExtract(ws,filename){
@@ -190,7 +212,16 @@ module.exports = function(RED){
             //var cmd = 'cd /home/root && ./'+filename;
             var cmd = 'cd '+rootDir+' && ./'+filename;
             console.log(cmd);
-            var ls = cp.exec(cmd);
+
+            var _options =  {
+                encoding: 'utf8',
+                timeout: 0,
+                maxBuffer: 1000*1024,
+                killSignal: 'SIGTERM',
+                cwd: null,
+                env: null
+            };
+            var ls = cp.exec(cmd,_options);
 
             ls.stdout.on('data', function (data) {
                 installStatus = 1;
@@ -208,6 +239,10 @@ module.exports = function(RED){
 
             ls.on('exit', function (code) {
                 console.log('child process exited with code ' + code);
+            });
+            
+            ls.on('close', function (code) {
+                console.log('child process exited with code ' + code);
                 if (installStatus == 1){
                     ws.send('{install_finished}安装成功！');
                 }
@@ -216,7 +251,6 @@ module.exports = function(RED){
                 }
             });
         }
- 
         if (ws_listen_status == 0){
             var WebSocketServer = require('ws').Server;
             wss = new WebSocketServer({port: 8081});
@@ -229,25 +263,22 @@ module.exports = function(RED){
                         var data = version.split('|');
                         var file = {'name':data[0],'size':data[1]}
                         ws.send('{download_start}开始下载新版本...');
-                        ftpDownLoad(ws,file);
+                        downLoad(ws,file);
                     }
                     else if (message === 'version_list'){
                         ws.send('{version_list_start}正在获取版本列表...');
-                        ftpGetVersionLists(ws);
+                        getVersionLists(ws);
                     }
                     else if (message.indexOf('{install}') != -1){
                         var version = message.replace('{install}','');
                         var data = version.split('|');
                         var file = {'name':data[0],'size':data[1]}
-                        //installSmartNode(ws);
                         install(ws,file);
                     }
                 });
             });
-        }    
-
+        }  
     }
-
 
     RED.nodes.registerType('upgrade', upgrade);
 }
